9e655a848466f8304e5f0f4d42000f21a8dbc80d
Forrest Stanley <1521744+fstanley@users.noreply.github.com>
2022-06-15 16:14:22 -0700

    Solution 0001 go

--- problems/0001.two_sum/two_sum.go
+++ problems/0001.two_sum/two_sum.go
@@ -1,5 +1,12 @@
 package leetcode
 
 func twoSum(nums []int, target int) []int {
-	return []int{0, 1}
+	for k0, v0 := range nums {
+		for k1, v1 := range nums[k0+1:] {
+			if v0+v1 == target {
+				return []int{k0, k1 + (k0 + 1)}
+			}
+		}
+	}
+	return []int{0, 0}
 }
